e2067bd9a78209cb53b8b373e202cc69cfbd1ee2
Bill Gauvey <bgauvey@gmail.com>
2015-02-16 07:02:46 -0500

    .

--- routes/ingredients.js
+++ routes/ingredients.js
@@ -3,11 +3,14 @@ module.exports = function (router, connection) {
 
     var ingredientsController = require('../controllers/ingredients');
     ingredientsController.set_db(connection);
-
+    
+    // things that call the path
     router
         .route('/ingredients/')
             .get(ingredientsController.getAll)
             .post(ingredientsController.add);
+    
+    // things that call the path with an id
     router
         .route('/ingredients/:id')
             .get(ingredientsController.getById)
